ce6685a06933d39c4c1448b28295c0bec8d6ba14
lewmilburn <lewiscmilburn@gmail.com>
2022-05-21 15:16:29 +0100

    Fixes issue #95

--- common/processes/security/ukauth.php
+++ common/processes/security/ukauth.php
@@ -4,7 +4,7 @@ function generate_uka_key()
 {
     try {
         $key = random_int(1000000000, 9999999999);
-        $hash = hash('sha3-512', $key);
+        $hash = hash(SECURITY_DEFAULT_HASH, $key);
     } catch (Exception $e) {
         echo 'Exception: '.$e;
         exit;
